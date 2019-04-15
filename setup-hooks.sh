93f1429f7230f3e30e381fc5f30295c20f1bf875
Joe Knapka <jaknapka+unter@gmail.com>
2019-04-15 16:37:29 -0600

    Added a script to set up git hooks.

---
+++ setup-hooks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set up git hooks for Unter/MVCA. This should be done firs
+# thing upon checking out the project source tree. It must
+# be run from the source tree root (the directory containing
+# this file).
+[[ -e src/tg/unter ]] || {
+	echo "Run this script from the root of the checked-out source tree."
+	exit 1
+}
+
+ln -s ../../run-tests.sh .git/hooks/pre-commit
+
+exit 0
